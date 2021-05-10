1295f723b709359368ecf4cf8d7c42e12871e01e
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2021-05-10 14:24:23 -0400

    FEAT: add python setup docs
    Added the configuration, setup.py, and supporting text documents for python module installation.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2020, the Aether Development Team (see doc/dev_team.md for members)
+# Full license can be found in LICENSE
+# -----------------------------------------------------------------------------
+
+from setuptools import setup
+
+
+# Run setup.  Setuptools will look for parameters in [metadata] section of
+# setup.cfg
+setup()

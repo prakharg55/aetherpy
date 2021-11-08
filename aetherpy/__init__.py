9339bc8d83ee07706ac9294c30344fff81ef1dc8
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2021-11-08 13:18:15 -0500

    STY: updated init files
    Updated the docstrings and import style in the package init files.

--- aetherpy/__init__.py
+++ aetherpy/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # Copyright 2020, the Aether Development Team (see doc/dev_team.md for members)
 # Full license can be found in License.md
+"""aetherpy package for ingesting and analysing model outputs."""
 
 # Define a logger object to allow easier log handling
 import logging
@@ -10,7 +11,9 @@ logging.raiseExceptions = False
 logger = logging.getLogger('aetherpy_logger')
 
 # Import the sub-modules
-from aetherpy import io, utils, plot
+from aetherpy import io
+from aetherpy import plot
+from aetherpy import utils
 
 # Define global variables
 vfile = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'version.txt')

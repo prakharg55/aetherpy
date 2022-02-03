4badc09f6317954870d0b13bbfa7942736b1bae5
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2022-02-03 11:52:49 -0500

    TST: added movie unit tests
    Finished adding movie unit tests.

--- aetherpy/tests/test_plot_movie.py
+++ aetherpy/tests/test_plot_movie.py
@@ -28,6 +28,7 @@ class TestMovie(object):
         self.fileext = ".png"
         self.filebase = "test_"
         self.tempfiles = []
+        self.moviename = "test.mp4"
 
         return
 
@@ -45,18 +46,37 @@ class TestMovie(object):
         self.tempdir.cleanup()
 
         # Clear the test environment attributes
-        del self.movie_dir, self.tempdir, self.tempfiles
+        del self.movie_dir, self.tempdir, self.tempfiles, self.moviename
         del self.fileext, self.filebase
         return
 
-    def make_files(self):
-        """Create a directory with temporary files."""
+    def make_files(self, data=False):
+        """Create a directory with temporary files.
+
+        Parameters
+        ----------
+        data : bool
+            Add data to the temporary file
+        """
         os.makedirs(self.movie_dir)
 
         for i in range(4):
+            # Create a temporary file that must be removed
             out = tempfile.mkstemp(suffix=self.fileext, prefix=self.filebase,
                                    dir=self.movie_dir)
-            self.tempfiles.append(out[1])
+
+            # Rename the temporary file to match the necessary format
+            goodname = os.path.join(self.movie_dir, "{:s}{:04d}{:s}".format(
+                self.filebase, i, self.fileext))
+            os.rename(out[1], goodname)
+
+            # Add data to the temporary file
+            if data:
+                with open(goodname, "wb") as fout:
+                    fout.write(b"AAAn")
+
+            # Save the good filename
+            self.tempfiles.append(goodname)
 
         return
 
@@ -68,10 +88,18 @@ class TestMovie(object):
 
         return
 
-    def test_setup_movie_dir_olddir(self):
-        """Test sucessful creation of a new directory for movie files."""
+    @pytest.mark.parametrize("wcard", ["*", "????"])
+    def test_setup_movie_dir_olddir(self, wcard):
+        """Test sucessful creation of a new directory for movie files.
+
+        Parameters
+        ----------
+        wcard : str
+            Accepted wildcard strings for the test files
+
+        """
         self.make_files()
-        file_glob = "*".join([self.filebase, self.fileext])
+        file_glob = "".join([self.filebase, wcard, self.fileext])
 
         img_names = mr.setup_movie_dir(self.movie_dir, file_glob=file_glob)
         assert os.path.isdir(self.movie_dir)
@@ -92,4 +120,78 @@ class TestMovie(object):
                                overwrite=False)
 
         assert str(ierr).find("files present in movie directory") >= 0
+        return
+
+    @pytest.mark.parametrize("rate", [30, 60])
+    def test_save_movie_nooverwrite_success(self, rate):
+        """Test the creation of a movie file in a clean directory.
+
+        Parameters
+        ----------
+        rate : int
+            Frame rate
+
+        """
+        # Set up the movie file directory
+        self.make_files(data=True)
+        image_files = os.path.join(self.movie_dir, "".join([
+            self.filebase, "%04d", self.fileext]))
+
+        # Create the move file
+        outfile = mr.save_movie(self.movie_dir, movie_name=self.moviename,
+                                image_files=image_files, rate=rate)
+
+        # Test the output
+        assert os.path.isfile(outfile), "movie file not created"
+
+        # Prepare for cleanup
+        self.tempfiles.append(outfile)
+        return
+
+    def test_save_movie_overwrite_success(self):
+        """Test the creation of a movie file when overwriting the old movie."""
+        # Set up the movie file directory
+        self.make_files(data=True)
+        image_files = os.path.join(self.movie_dir, "".join([
+            self.filebase, "%04d", self.fileext]))
+
+        # Make an output file with the same name as the movie file
+        out = tempfile.mkstemp(suffix=self.fileext, prefix=self.filebase,
+                               dir=self.movie_dir)
+        os.rename(out[1], os.path.join(self.movie_dir, self.moviename))
+
+        # Create the move file
+        outfile = mr.save_movie(self.movie_dir, movie_name=self.moviename,
+                                image_files=image_files, overwrite=True)
+
+        # Test the output
+        assert os.path.isfile(outfile), "movie file not created"
+
+        # Prepare for cleanup
+        self.tempfiles.append(outfile)
+        return
+
+    def test_save_movie_overwrite_blocking(self):
+        """Test raises IOError when a movie exists and overwrite is blocked."""
+        # Set up the movie file directory
+        self.make_files(data=True)
+        image_files = os.path.join(self.movie_dir, "".join([
+            self.filebase, "%04d", self.fileext]))
+
+        # Make an output file with the same name as the movie file
+        out = tempfile.mkstemp(suffix=self.fileext, prefix=self.filebase,
+                               dir=self.movie_dir)
+        outfile = os.path.join(self.movie_dir, self.moviename)
+        os.rename(out[1], outfile)
+
+        # Create the move file
+        with pytest.raises(IOError) as ierr:
+            mr.save_movie(self.movie_dir, movie_name=self.moviename,
+                                image_files=image_files, overwrite=False)
+
+        # Test the output
+        assert str(ierr).find('already exists') >= 0, "unexpected IOError"
+
+        # Prepare for cleanup
+        self.tempfiles.append(outfile)
         return
